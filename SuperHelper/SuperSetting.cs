fbdb8da318f403db156f0261f2472e5e0aa05922
一 辛 <Archi-BC@outlook.com>
2024-04-16 09:33:39 +0800

    2.0.1

--- SuperHelper/SuperSetting.cs
+++ SuperHelper/SuperSetting.cs
@@ -1,12 +1,40 @@
 ﻿using SimpleGrasshopper.Attributes;
+using System;
 
 namespace SuperHelper;
 
 public static partial class SuperSetting
 {
-    [Config("SuperHelper","SuperHelper"), ToolButton("C:\\Users\\dell\\OneDrive\\vsProject\\SuperHelper\\SuperHelper\\Resources\\SuperHelperIcon_24.png")]
+    [Config("Open"), ToolButton("C:\\Users\\dell\\OneDrive\\vsProject\\SuperHelper\\SuperHelper\\Resources\\SuperHelperIcon_24.png")]
     [Setting]
     private static readonly bool open = true;
+
+    [Config("CapsuleHighlight")]
+    public static bool CapsuleHighlight
+    {         
+        get => Grasshopper.CentralSettings.CapsuleHighlight;
+        set 
+        {
+            Grasshopper.CentralSettings.CapsuleHighlight = value;
+            OnCapsuleHighlightChanged?.Invoke(value);
+        }
+    }
+
+    public static event Action<bool> OnCapsuleHighlightChanged;
+
+    [Config("CapsuleShine")]
+    public static bool CapsuleShine
+    {
+        get => Grasshopper.CentralSettings.CapsuleShine;
+        set
+        {
+            Grasshopper.CentralSettings.CapsuleShine = value;
+            OnCapsuleShineChanged?.Invoke(value);
+        }
+    }
+
+    public static event Action<bool> OnCapsuleShineChanged;
+
 
     static SuperSetting()
     {
